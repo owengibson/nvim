8fc1d3921f25ee7c4cfab78159e372dfcce1d3ec
owengibson <114120616+owengibson@users.noreply.github.com>
2025-04-30 22:13:39 +1000

    Edit keymaps.lua to add cursor nav keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,3 +10,6 @@ keymap({"n", "v"}, "<leader>pc", "\"+p", opts) -- Paste from system clipboard
 keymap({"n", "v"}, "<leader>pf", "ggVG\"+p", opts) -- Paste entire file from system clipboard
 
 keymap("n", "<leader>a", "ggVG", opts) -- Select all
+
+keymap({"n", "v"}, "H", "^", opts) -- Cursor to first non-whitespace character of line
+keymap({"n", "v"}, "L", "$", opts) -- Cursor to end of line
