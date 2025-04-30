ad24d5d4ce752e86368c9b68cdc00b7742cca0fb
owengibson <114120616+owengibson@users.noreply.github.com>
2025-04-30 16:31:35 +1000

    Initial commit 	new file:   init.lua 	new file:   lua/user/keymaps.lua 	new file:   lua/user/options.lua

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,12 @@
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap({"n", "v"}, "<leader>yc", "\"+y", opts) -- Copy to system clipboard
+keymap({"n", "v"}, "<leader>yf", "<cmd>%y+<cr>", opts) -- Copy entire file to system clipboard
+keymap({"n", "v"}, "<leader>pc", "\"+p", opts) -- Paste from system clipboard
+keymap({"n", "v"}, "<leader>pf", "ggVG\"+p", opts) -- Paste entire file from system clipboard
+
+keymap("n", "<leader>a", "ggVG", opts) -- Select all
